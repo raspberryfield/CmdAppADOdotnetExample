e75ebb5316b95b93f6b96bfc04b2ce96e827c16c
Fred <my@email.com>
2020-07-30 16:47:06 +0200

    Menu is working.

---
+++ ConsoleApp1/ConsoleApp1/InputHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    static class InputHandler
+    {
+        public static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("MAIN MENU:");
+            Console.WriteLine("1. Enter connection string.");
+            UserOptionMainMenu();
+        }
+
+        private static void UserOptionMainMenu()
+        {
+            try
+            {
+                int userOption = Convert.ToInt32(Console.ReadLine());
+                switch (userOption)
+                {
+                    case 1:
+                        ConnectionMenu();
+                        break;
+
+                    default:
+                        Console.WriteLine("Test test test.");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invalid input...");
+                Thread.Sleep(1000);
+                MainMenu();
+            }
+        }
+
+        private static void ConnectionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("CONNECTION MENU:");
+            try
+            {
+                Console.Write("Name of server: ");
+                var server = Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+
+            }
+        }  
+
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 
+/*This console application provides a little command line interface to test you database connaction.*/
+
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintGreeting();
+
+
+            InputHandler.MainMenu();
+            
+            
+
+            Console.ReadLine();
+        }
+
+        private static void PrintGreeting()
+        {
+            Console.WriteLine("ADO.NET test program v.0.1. -FF. ");
+            Console.WriteLine("Hi, this is an command line line application to test functionality in ADO.NET.");
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.WriteLine("Press [Enter] to continue...");
+            Console.ReadLine();
         }
     }
 }
